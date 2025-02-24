4fd800bdeed641f195cfb127c217bfa5ad3fc06e
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2025-02-24 15:27:12 +0100

    test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,6 @@
-'use client';
+"use client";
 import { getAllProducts } from "@/actions/actions";
+import { Suspense } from "react";
 import Filter from "@/components/Filter";
 import ListProducts from "@/components/ListProducts";
 import Pagination from "@/components/Pagination";
@@ -76,80 +77,82 @@ export default function Products() {
   ]);
 
   return (
-    <div>
-      <div className="w-full h-[20vh] xl:h-[35vh]   mb-[30px] relative bg-[url(/BannerShop.png)] bg-[80%] bg-cover ">
-        <div className="flex flex-col items-center  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <div className="flex flex-col items-center md:b-[25px] md:pb-[20px]">
-            <h1 className="text-[35px] font-[600] lg:text-[55px] ">
-              {category}
-            </h1>
-            <div className="flex gap-2 items-center  text-[15px] lg:text-[17px]">
-              <h2>Products</h2>
-              <ChevronRight />
-              <h2 className="">{category}</h2>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div>
+        <div className="w-full h-[20vh] xl:h-[35vh]   mb-[30px] relative bg-[url(/BannerShop.png)] bg-[80%] bg-cover ">
+          <div className="flex flex-col items-center  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <div className="flex flex-col items-center md:b-[25px] md:pb-[20px]">
+              <h1 className="text-[35px] font-[600] lg:text-[55px] ">
+                {category}
+              </h1>
+              <div className="flex gap-2 items-center  text-[15px] lg:text-[17px]">
+                <h2>Products</h2>
+                <ChevronRight />
+                <h2 className="">{category}</h2>
+              </div>
+            </div>
+            <div className="">
+              <ul className="flex gap-[14px]  lg:gap-[20px]">
+                <li
+                  id="All"
+                  onClick={() => setCategory("All")}
+                  className={`text-[17px] lg:text-[20px] cursor-pointer ${
+                    category === "All" ? "border-b-[1.5px] border-black" : ""
+                  }`}
+                >
+                  All
+                </li>
+                {categories.map((cate: Category) => {
+                  return (
+                    <li
+                      onClick={() => {
+                        setCategory(cate.name);
+                        setPage(1);
+                        setSearch("");
+                      }}
+                      key={cate._id}
+                      id={cate._id}
+                      className={`text-[17px] lg:text-[20px] cursor-pointer ${
+                        cate.name === category
+                          ? "border-b-[1.5px] border-black"
+                          : ""
+                      }`}
+                    >
+                      {cate.name}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
-          <div className="">
-            <ul className="flex gap-[14px]  lg:gap-[20px]">
-              <li
-                id="All"
-                onClick={() => setCategory("All")}
-                className={`text-[17px] lg:text-[20px] cursor-pointer ${
-                  category === "All" ? "border-b-[1.5px] border-black" : ""
-                }`}
-              >
-                All
-              </li>
-              {categories.map((cate: Category) => {
-                return (
-                  <li
-                    onClick={() => {
-                      setCategory(cate.name);
-                      setPage(1);
-                      setSearch("");
-                    }}
-                    key={cate._id}
-                    id={cate._id}
-                    className={`text-[17px] lg:text-[20px] cursor-pointer ${
-                      cate.name === category
-                        ? "border-b-[1.5px] border-black"
-                        : ""
-                    }`}
-                  >
-                    {cate.name}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
         </div>
-      </div>
-      <Filter
-        stock={stock}
-        setStock={setStock}
-        category={category}
-        setCategory={setCategory}
-        priceRange={priceRange}
-        setPriceRange={setPriceRange}
-        size={size}
-        setSize={setSize}
-      />
-      {/* <div>~
+        <Filter
+          stock={stock}
+          setStock={setStock}
+          category={category}
+          setCategory={setCategory}
+          priceRange={priceRange}
+          setPriceRange={setPriceRange}
+          size={size}
+          setSize={setSize}
+        />
+        {/* <div>~
          <h3><span>{products}</span> Products Found</h3>
       </div> */}
-      <ListProducts
-        products={products}
-        loading={loading}
-        limit={Number(limit)}
-      />
-      {Number(totalPages) > 1 && (
-        <Pagination
-          currentPage={Number(page)}
-          totalPages={Number(totalPages)}
-          setPage={setPage}
-          page={Number(page)}
+        <ListProducts
+          products={products}
+          loading={loading}
+          limit={Number(limit)}
         />
-      )}
-    </div>
+        {Number(totalPages) > 1 && (
+          <Pagination
+            currentPage={Number(page)}
+            totalPages={Number(totalPages)}
+            setPage={setPage}
+            page={Number(page)}
+          />
+        )}
+      </div>
+    </Suspense>
   );
 }
