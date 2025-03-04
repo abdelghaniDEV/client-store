78d825c8f71823e225429c57bda41606736dd6e6
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2025-03-05 00:34:23 +0100

    handel add cart

--- src/components/FooterMobile.tsx
+++ src/components/FooterMobile.tsx
@@ -1,28 +1,46 @@
-import { Heart, LayoutDashboard, Search, ShoppingBag } from 'lucide-react'
-import React from 'react'
+"use client";
+import { RootState } from "@/redux/store";
+import { Heart, LayoutDashboard, Search, ShoppingBag } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function FooterMobile() {
+  const wishlist = useSelector(
+    (statu: RootState) => statu.wishlist.items.length
+  );
+  const cart = useSelector((statu: RootState) => statu.cart.items.length);
   return (
-    <div className='fixed bottom-0 z-[1000] bg-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] md:hidden w-full pt-2'>
-      <div className='grid grid-cols-4 items-center container'>
-        <div className='flex flex-col items-center '>
-          <LayoutDashboard className='w-5 h-5' />
+    <div className="fixed bottom-0 z-[1000] bg-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] md:hidden w-full pt-2">
+      <div className="grid grid-cols-4 items-center container">
+        <div className="flex flex-col items-center ">
+          <LayoutDashboard className="w-5 h-5" />
           <span>Shop</span>
         </div>
-        <div className='flex flex-col items-center '>
-          <Search className='w-5 h-5' />
+        <div className="flex flex-col items-center ">
+          <Search className="w-5 h-5" />
           <span>Search</span>
         </div>
-        <div className='flex flex-col items-center '>
-          <Heart className='w-5 h-5' />
+        <Link
+          href={"/wishlist"}
+          className="flex flex-col items-center relative "
+        >
+          <Heart className="w-5 h-5" />
           <span>Wishlist</span>
-        </div>
-        <div className='flex flex-col items-center '>
-          <ShoppingBag className='w-5 h-5' />
+          <div className="w-4 h-4 rounded-full bg-main-secondary flex items-center justify-center absolute top-[-4px] right-[16px]">
+            <span className="  text-white text-[13px] font-[500]">
+              {wishlist}
+            </span>
+          </div>
+        </Link>
+        <div className="flex flex-col items-center relative ">
+          <ShoppingBag className="w-5 h-5" />
           <span>Cart</span>
+          <div className="w-4 h-4 rounded-full bg-main-secondary flex items-center justify-center absolute top-[-4px] right-[16px]">
+            <span className="  text-white text-[13px] font-[500]">{cart}</span>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
