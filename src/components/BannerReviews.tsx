c21ce4f841857c441da50bc797d7f9326f8e217f
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2025-03-05 17:07:48 +0100

    test

--- src/components/BannerReviews.tsx
+++ src/components/BannerReviews.tsx
@@ -7,7 +7,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { reviewsItem } from "@/types";
-import ReviewCart from "./ReviewCart";
+
 import TitleHome from "./ui/TitleHome";
 
 export default function BannerReviews() {
