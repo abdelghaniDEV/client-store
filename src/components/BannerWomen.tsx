a1958f05e4272d853f6fe605467c29477d429e5b
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2025-03-05 19:28:52 +0100

    change style footer

--- src/components/BannerWomen.tsx
+++ src/components/BannerWomen.tsx
@@ -6,27 +6,28 @@ import { ArrowRight } from "lucide-react";
 
 export default function BannerWomen() {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-20 container items-center py-6">
-      <div  className="w-full md:h-[600px]">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-20 container items-center py-6">
+      <div className="w-full lg:h-[600px]">
         <Image
           src={imgHijab}
           alt="hijab"
           width={100}
           height={400}
           unoptimized
-         className="bg-cover w-full md:h-[600px]"
+          className="bg-cover w-full lg:h-[600px]"
         />
       </div>
       <div className=" flex flex-col items-center md:items-start md:justify-start justify-center">
         <span className="md:text-[30px] text-[20px] font-[500] md:font-normal ">
-        Up To 40% Off
+          Up To 40% Off
         </span>
         <h1 className="text-[35px] pt-2 leading-[30px] md:text-[60px] md:leading-[60px]  ">
-        Fashion T-Shirt <br/>
-        for Women
+          Fashion T-Shirt <br />
+          for Women
         </h1>
         <p className="text-[18px] text-center md:text-start">
-        What you wear often shows what kind of person you are.Show a different personality you dont have with your clothing.
+          What you wear often shows what kind of person you are.Show a different
+          personality you dont have with your clothing.
         </p>
         <div className=" ">
           <Button className="bg-main-secondary text-white  mt-4 md:text-[16px] rounded-none  md:w-auto flex items-center   gap-2">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,82 +1,86 @@
+"use client";
 import React from "react";
 import { Facebook, Instagram, Linkedin, Mail, Phone } from "lucide-react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { Category } from "@/types";
+import Link from "next/link";
 
 export default function Footer() {
+  const categories = useSelector((statu: RootState) => statu.categories);
   return (
     <div className="bg bg-main-primary w-full  text-black  ">
-      <div className="grid md:grid-cols-3 grid-cols-1 gap-4  container">
+      <div className="flex gap-4 flex-col lg:flex-row  container">
         <div>
-          <p className="lg:w-[300px] pt-5 uppercase text-[25px] font-[600] leading-[22px]">
+          <p className="lg:w-[300px] uppercase text-[25px] md:text-[35px] md:leading-[35px] lg:text-[25px] font-[600] leading-[22px]">
             Get Creative white clothes : <br /> stylish inspiration for every
             occation .
           </p>
+          <div className="flex items-center gap-4 pt-3">
+            <Link href="#" className=" hover:text-gray-500">
+              <Facebook className="w-6 h-6" />
+            </Link>
+            <Link href="#" className=" hover:text-gray-500">
+              <Instagram className="w-6 h-6" />
+            </Link>
+            <Link href="#" className=" hover:text-gray-500">
+              <Linkedin className="w-6 h-6" />
+            </Link>
+            <Link href="#" className=" hover:text-gray-500">
+              <Mail className="w-6 h-6" />
+            </Link>
+          </div>
         </div>
-
-        <div className="">
-          <h4 className="pb-1 text-[20px] font-[500] text-">Categories</h4>
-          <ul className="flex flex-col text-[18px] gap-2">
-            <li>Men</li>
-            <li>Women</li>
-            <li>Shoes</li>
-            <li>Accesoire</li>
-            {/* <li>QR Code Integration for Easy Access</li> */}
-          </ul>
-        </div>
-        <div className="">
-          <h4 className="pb-1 text-[20px] font-[500]">Pages</h4>
-          <ul className="flex flex-col text-[18px] gap-2">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Shop</a>
-            </li>
-            <li>
-              <a href="#">Products</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Contact Us</a>
-            </li>
-          </ul>
+        <div className="flex gap-20 md:gap-[100px]">
+          <div>
+            <h4 className="pb-1 text-[20px] font-[500] text-">Pages</h4>
+            <ul className="flex flex-col text-[18px] gap-2">
+              <li>Home</li>
+              <li>Shop</li>
+              <li>Products</li>
+              <li>About Us</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="pb-1 text-[20px] font-[500] text-">Contact Info</h4>
+            <div className="flex flex-col gap-3">
+              <div>
+                <h5 className="font-[500]">Address : </h5>
+                <p>123 Street, City, Country</p>
+              </div>
+              <div>
+                <h5 className="font-[500]">Email : </h5>
+                <p>Example@gmail.com</p>
+              </div>
+              <div>
+                <h5 className="font-[500]">Working Days/Hours:</h5>
+                <p>Mon-Sat / 8:00-18:00</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
-          <h4 className="pb-4 text-[20px] text-">Get In Touch</h4>
-          <div className="flex gap-2 mb-2 items-center">
-            <Mail className="w-4 h-4" />
-            <p className="">denioMenu@gmail.com</p>
-          </div>
-          <div className="flex gap-2 mb-2 items-center">
-            <Phone className="w-4 h-4" />
-            <p className="">+212636998077</p>
-          </div>
-          <ul className="flex  gap-1 text- items-center">
-            <li className="b  p-[5px] rounded-[10px] ">
-              <Linkedin className="h-[20px] w-[20px]" />
-            </li>
-            <li className="b p-[5px] rounded-[10px] ">
-              <Instagram className="h-[20px] w-[20px]" />
-            </li>
-            <li className="b  p-[5px] rounded-[10px] ">
-              <Facebook className="h-[20px] w-[20px]" />
-            </li>
+          <h4 className="pb-1 text-[20px] font-[500] text-">Categories</h4>
+          <ul className="flex items-center flex-wrap gap-4">
+            {categories.map((category: Category) => {
+              return (
+                <li
+                  className="border-[1px] py-1 px-3 border-main-secondary"
+                  key={category._id}
+                >
+                  {category.name}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
-      <hr />
-      <div className="md:flex text-center justify-between pt-10">
-        <div>
-          <p>@2025 BagStor, All rights Reserved </p>
-        </div>
-        <div>
-          <ul className="flex gap-2 justify-center">
-            <li>Terms & Conditions</li>
-            <li>FAQ</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </div>
+      <div className="pt-4">
+        <p className="text-[14px] text-center text-gray-500">
+          &copy; 2022 All rights reserved. Designed by{" "}
+          <span className="text-main-text">BAGSTORE</span>
+        </p>
       </div>
     </div>
   );
